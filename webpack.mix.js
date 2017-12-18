2269c68212fe9b8c17b60353eae6660529e9e6b5
HoangLN <hoang.ln@neo-lab.vn>
2017-12-18 11:10:46 +0700

    update laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,23 @@
 let mix = require('laravel-mix');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 mix.js('src/js/app.js', 'public/js');
 mix.sass('src/sass/app.scss', 'public/css');
 mix.copyDirectory('src/libs', 'public/libs');
 mix.copyDirectory('src/img', 'public/img');
+
+if (mix.inProduction()) {
+    mix.webpackConfig({
+        plugins: [
+            new CompressionPlugin({
+                asset: '[path]',
+                algorithm: 'gzip',
+                test: /\.js$|\.css$/,
+                threshold: 10240,
+                minRatio: 0.8
+            }),
+        ]
+    });
+
+    mix.version();
+}
